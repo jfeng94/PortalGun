c3725af7d65df0399706ecd1e3f9db391fcf8c24
Jerry Feng <jfeng94@gmail.com>
2016-02-02 01:30:56 -0800

    Started app. Added button with sector numbers

---
+++ PortalGun/Assets/Scripts/EventHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class EventHandler : MonoSingleton<EventHandler> {
+
+    public delegate void ClickAction();
+
+    public event ClickAction OnSetSector;
+
+    public void TriggerSetSector() {
+        if (OnSetSector != null) {
+            OnSetSector();
+        }
+    }
+}
---
+++ PortalGun/Assets/Scripts/Master.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Master : MonoSingleton <Master> {
+    public Dropdown linkedSector;
+    private Dropdown storedSector;
+
+    private int sectorValue;
+    private string sectorName;
+
+    void Start() {
+        Debug.Log("Hello World");
+
+        EventHandler.instance.OnSetSector += SetSector;
+    }
+
+    public void SetSector() {
+        sectorValue = linkedSector.value;
+        sectorName  = linkedSector.options[linkedSector.value].text;
+        Debug.Log("Sector is " + sectorValue + "\t" +
+                  "Sector name is " + sectorName);
+
+    }
+
+}
